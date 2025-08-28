91454d3fb01f645740e3bae6a722babe714f323b
Daniël de Kok <me@danieldk.eu>
2025-08-28 11:29:33 +0200

    cutlass_4_0: init at 4.0.0 (#86)

--- pkgs/cutlass/default.nix
+++ pkgs/cutlass/default.nix
@@ -28,4 +28,8 @@ in
     version = "3.9.2";
     hash = "sha256-teziPNA9csYvhkG5t2ht8W8x5+1YGGbHm8VKx4JoxgI=";
   };
+  cutlass_4_0 = builder {
+    version = "4.0.0";
+    hash = "sha256-HJY+Go1viPkSVZPEs/NyMtYJzas4mMLiIZF3kNX+WgA=";
+  };
 }
